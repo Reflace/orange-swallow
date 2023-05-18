97cec1f45cb03dce1d01af488b1480be87c163b6
Maksim Bidenko <m.bidenko@bk.ru>
2023-05-18 16:20:03 +0300

    Поменял настройки размера окна

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle('Orange Swallow')
-        self.setFixedSize(800, 600)  # Фиксированный размер
+        self.setWindowTitle('Turbo Waffle')
+        self.setMinimumSize(QSize(800, 600))  # Минимальный размер
+        # self.setFixedSize(QSize(800, 600))  # Фиксированный размер
 
 
 app = QApplication(sys.argv)
