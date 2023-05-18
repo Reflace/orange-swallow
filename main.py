18db4df3be33950ed0e0190a18db01336e1fe6da
Maksim Bidenko <m.bidenko@bk.ru>
2023-05-18 20:51:18 +0300

    Простая разметка главного окна

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 import sys
 
-from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt5.QtCore import QSize
+from PyQt5.QtWidgets import (
+    QApplication,
+    QHBoxLayout,
+    QLabel,
+    QWidget,
+)
 
 
-class MainWindow(QMainWindow):
+class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle('Turbo_Waffle')
         self.setMinimumSize(QSize(800, 600))  # Минимальный размер
-        # self.setFixedSize(QSize(800, 600))  # Фиксированный размер
+
+        main_layout = QHBoxLayout()  # Главный main_layout, остальное на нем будет
+
+        main_layout.addWidget(QLabel('Фильтры и поиск'), stretch=1)
+        main_layout.addWidget(QLabel('Таблица с фильмами'), stretch=2)
+
+        self.setLayout(main_layout)
 
 
-app = QApplication(sys.argv)
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
 
-window = MainWindow()
-window.show()
+    window = MainWindow()
+    window.show()
 
-app.exec()  # Запускаем основной рабочий цикл
+    app.exec()  # Запускаем основной рабочий цикл
